68ddac42b56b90b419c436833a067f1f8fc981f9
khactrung0902 <khanhtrung13@gmail.com>
2021-11-01 20:01:34 +0700

    first

---
+++ src/components/QrCode.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Vue QrCode</h1>
+    <qrcode-vue 
+      :value="value" 
+      :size="size" 
+      level="H" 
+    />
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        value: 'https://www.google.com',
+        size: 150,
+      }
+    }
+  }
+</script>
